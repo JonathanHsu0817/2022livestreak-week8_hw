b031878541de59b616d0cc800431dd82ff41b8af
Jonathan0817 <00339028@email.ntou.edu.tw>
2022-09-14 22:10:02 +0800

    Update 2022-09-14T14:09:58.462Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,49 @@
 "use strict";
 
-var swiper = new Swiper('.artists-swiper', {
-  // 分頁   
+var indexSwiper = new Swiper(".index-swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000
+  },
+  centeredSlides: true,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.3
+    },
+    1200: {
+      slidesPerView: 2.88
+    },
+    1400: {
+      slidesPerView: 2.9
+    }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var introSwiper = new Swiper(".intro-swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 4
+    }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
   }
 });
 //# sourceMappingURL=all.js.map
