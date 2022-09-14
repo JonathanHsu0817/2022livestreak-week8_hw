37c687a9df127aa653db3848ee6c0225e30c376f
Jonathan0817 <00339028@email.ntou.edu.tw>
2022-09-14 22:09:21 +0800

    初步完成index&intro

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,47 @@
-const swiper = new Swiper('.artists-swiper', {
-  // 分頁   
+const indexSwiper = new Swiper(".index-swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+  },
+  centeredSlides: true,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.3
+    },
+    1200: {
+      slidesPerView: 2.88
+    },
+    1400: {
+      slidesPerView: 2.9
+    }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
   pagination: {
-    el: '.swiper-pagination',
-  }
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+});
+
+const introSwiper = new Swiper(".intro-swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 4
+    },
+  },
+  spaceBetween: 24,
+  effect: 'slide',
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
 });
